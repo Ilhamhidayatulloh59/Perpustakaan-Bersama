abf619d64497e0ddacd69f928571042d59a254e6
fachrizahidaya <fachriza.theblues@gmail.com>
2022-11-16 22:47:52 +0700

    router for multer

--- back-end/routers/book.js
+++ back-end/routers/book.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { book } = require("../controllers");
+const { multerUpload } = require("../helpers/multer");
 
 router.post("/create", book.create);
 router.get("/list", book.getAll);
@@ -10,5 +11,6 @@ router.patch("/list", book.update);
 router.get("/list/total", book.totalBooks);
 router.get("/search", book.searchBy);
 router.get("/sort", book.sortBy);
+router.post("/uploaded/:id", multerUpload.single("file"), book.uploadFile)
 
 module.exports = router;
